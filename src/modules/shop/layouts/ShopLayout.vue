fe823ce4deb3cfaa8028d2e841545c13e2903882
Sergio Barreras <sergiobg.isc@gmail.com>
2024-10-25 14:42:34 -0700

    style: diseño de la tienda

---
+++ src/modules/shop/layouts/ShopLayout.vue
@@ -0,0 +1,18 @@
+<template>
+  <!-- component -->
+  <!-- Create By Joker Banny -->
+
+  <!-- Header Navbar -->
+  <TopMenu />
+
+  <!-- RouterView -->
+  <RouterView />
+
+  <!-- Footer -->
+  <CustomFooter />
+</template>
+
+<script lang="ts" setup>
+import CustomFooter from '../components/CustomFooter.vue';
+import TopMenu from '../components/TopMenu.vue';
+</script>
