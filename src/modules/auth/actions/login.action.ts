d5cd7e446d6bf077e1020affa529e54bab6d72b2
Sergio Barreras <sergiobg.isc@gmail.com>
2024-10-26 11:35:22 -0700

    feat: login action

---
+++ src/modules/auth/actions/login.action.ts
@@ -0,0 +1,43 @@
+import { tesloApi } from '@/api/tesloApi';
+import { isAxiosError } from 'axios';
+import type { User } from '../interfaces/user.interface';
+import type { AuthResponse } from '../models/auth.response';
+
+interface LoginError {
+  ok: false;
+  message: string;
+}
+
+interface LoginSuccess {
+  ok: true;
+  user: User;
+  token: string;
+}
+
+export const loginAction = async (
+  email: string,
+  password: string,
+): Promise<LoginSuccess | LoginError> => {
+  try {
+    const { data } = await tesloApi.post<AuthResponse>('/auth/login', {
+      email,
+      password,
+    });
+
+    return {
+      ok: true,
+      user: data.user,
+      token: data.token,
+    };
+  } catch (error) {
+    if (isAxiosError(error) && error.response?.status === 401) {
+      return {
+        ok: false,
+        message: error.message,
+      };
+    }
+
+    console.log(error);
+    throw new Error('Something bad happen');
+  }
+};
