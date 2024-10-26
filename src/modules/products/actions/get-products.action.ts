7a5ac87b97ce0f71160e886d8f00f2a7acacc97a
Sergio Barreras <sergiobg.isc@gmail.com>
2024-10-26 00:27:33 -0700

    feat: mostrar productos en pantalla

--- src/modules/products/actions/get-products.action.ts
+++ src/modules/products/actions/get-products.action.ts
@@ -2,7 +2,10 @@ import { tesloApi } from '@/api/tesloApi';
 import type { Product } from '../interfaces/product.interface';
 import { getProductImageAction } from './get-product-image.action';
 
-export const getProductsAction = async (page: number = 1, limit: number = 10) => {
+export const getProductsAction = async (
+  page: number = 1,
+  limit: number = 10,
+): Promise<Product[]> => {
   try {
     const { data } = await tesloApi.get<Product[]>(
       `/products?limit=${limit}&offset=${page * limit}`,
