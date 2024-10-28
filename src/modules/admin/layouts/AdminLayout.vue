a2f549d92ed24efb8b9882edb50e1495b9c34163
Sergio Barreras <sergiobg.isc@gmail.com>
2024-10-28 09:07:59 -0700

    style: componentes que no se utilizan comentados

--- src/modules/admin/layouts/AdminLayout.vue
+++ src/modules/admin/layouts/AdminLayout.vue
@@ -3,7 +3,11 @@
   <div class="flex w-screen h-screen text-gray-700">
     <!-- Component Start -->
     <div class="flex flex-col items-center w-16 pb-4 overflow-auto border-r border-gray-300">
-      <a class="flex items-center justify-center flex-shrink-0 w-full h-16 bg-gray-300" href="#">
+      <RouterLink
+        to="/"
+        class="flex items-center justify-center flex-shrink-0 w-full h-16 bg-gray-300"
+        href="#"
+      >
         <svg
           class="w-8 h-8"
           xmlns="http://www.w3.org/2000/svg"
@@ -18,8 +22,8 @@
             d="M20 7l-8-4-8 4m16 0l-8 4m8-4v10l-8 4m0-10L4 7m8 4v10M4 7v10l8 4"
           />
         </svg>
-      </a>
-      <a
+      </RouterLink>
+      <!-- <a
         class="flex items-center justify-center flex-shrink-0 w-10 h-10 mt-4 rounded hover:bg-gray-300"
         href="#"
       >
@@ -37,8 +41,8 @@
             d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
           />
         </svg>
-      </a>
-      <a
+      </a> -->
+      <!-- <a
         class="flex items-center justify-center flex-shrink-0 w-10 h-10 mt-4 rounded hover:bg-gray-300"
         href="#"
       >
@@ -56,8 +60,8 @@
             d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z"
           />
         </svg>
-      </a>
-      <a
+      </a> -->
+      <!-- <a
         class="flex items-center justify-center flex-shrink-0 w-10 h-10 mt-4 rounded hover:bg-gray-300"
         href="#"
       >
@@ -75,8 +79,8 @@
             d="M20 13V6a2 2 0 00-2-2H6a2 2 0 00-2 2v7m16 0v5a2 2 0 01-2 2H6a2 2 0 01-2-2v-5m16 0h-2.586a1 1 0 00-.707.293l-2.414 2.414a1 1 0 01-.707.293h-3.172a1 1 0 01-.707-.293l-2.414-2.414A1 1 0 006.586 13H4"
           />
         </svg>
-      </a>
-      <a
+      </a> -->
+      <!-- <a
         class="flex items-center justify-center flex-shrink-0 w-10 h-10 mt-4 rounded hover:bg-gray-300"
         href="#"
       >
@@ -94,8 +98,8 @@
             d="M16 8v8m-4-5v5m-4-2v2m-2 4h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z"
           />
         </svg>
-      </a>
-      <a
+      </a> -->
+      <!-- <a
         class="flex items-center justify-center flex-shrink-0 w-10 h-10 mt-4 rounded hover:bg-gray-300"
         href="#"
       >
@@ -113,8 +117,8 @@
             d="M12 6V4m0 2a2 2 0 100 4m0-4a2 2 0 110 4m-6 8a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4m6 6v10m6-2a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4"
           />
         </svg>
-      </a>
-      <a
+      </a> -->
+      <!-- <a
         class="flex items-center justify-center flex-shrink-0 w-10 h-10 mt-4 mt-auto rounded hover:bg-gray-300"
         href="#"
       >
@@ -132,11 +136,11 @@
             d="M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z"
           />
         </svg>
-      </a>
+      </a> -->
     </div>
     <div class="flex flex-col w-56 border-r border-gray-300">
       <button class="relative text-sm focus:outline-none group">
-        <div
+        <!-- <div
           class="flex items-center justify-between w-full h-16 px-4 border-b border-gray-300 hover:bg-gray-300"
         >
           <span class="font-medium"> Dropdown </span>
@@ -152,7 +156,7 @@
               clip-rule="evenodd"
             />
           </svg>
-        </div>
+        </div> -->
         <div
           class="absolute z-10 flex-col items-start hidden w-full pb-1 bg-white shadow-lg group-focus:flex"
         >
@@ -200,17 +204,17 @@
     <div class="flex flex-col flex-grow">
       <div class="flex items-center flex-shrink-0 h-16 px-8 border-b border-gray-300">
         <h1 class="text-lg font-medium">{{ authStore.user?.fullName }}</h1>
-        <button
+        <!-- <button
           class="flex items-center justify-center h-10 px-4 ml-auto text-sm font-medium rounded hover:bg-gray-300"
         >
           Action 1
-        </button>
-        <button
+        </button> -->
+        <!-- <button
           class="flex items-center justify-center h-10 px-4 ml-2 text-sm font-medium bg-gray-200 rounded hover:bg-gray-300"
         >
           Action 2
-        </button>
-        <button class="relative ml-2 text-sm focus:outline-none group">
+        </button> -->
+        <!-- <button class="relative ml-2 text-sm focus:outline-none group">
           <div class="flex items-center justify-between w-10 h-10 rounded hover:bg-gray-300">
             <svg
               class="w-5 h-5 mx-auto"
@@ -234,7 +238,7 @@
             <a class="w-full px-4 py-2 text-left hover:bg-gray-300" href="#">Menu Item 1</a>
             <a class="w-full px-4 py-2 text-left hover:bg-gray-300" href="#">Menu Item 1</a>
           </div>
-        </button>
+        </button> -->
       </div>
 
       <RouterView />
@@ -242,7 +246,7 @@
     <!-- Component End  -->
   </div>
 
-  <a
+  <!-- <a
     class="fixed flex items-center justify-center h-8 pr-2 pl-1 bg-blue-600 rounded-full bottom-0 right-0 mr-4 mb-4 shadow-lg text-blue-100 hover:bg-blue-600"
     href="https://twitter.com/lofiui"
     target="_top"
@@ -257,7 +261,7 @@
       </svg>
     </div>
     <span class="text-sm ml-1 leading-none">@lofiui</span>
-  </a>
+  </a> -->
 </template>
 
 <script lang="ts" setup>
--- src/modules/shop/components/TopMenu.vue
+++ src/modules/shop/components/TopMenu.vue
@@ -86,38 +86,19 @@
         class="hidden w-full items-center justify-between md:order-1 md:flex md:w-auto"
         id="navbar-sticky"
       >
+        <!-- TODO: Pensar que poner aquí -->
+
         <ul
           class="mt-4 flex flex-col rounded-lg border border-gray-100 bg-gray-50 p-4 md:mt-0 md:flex-row md:space-x-8 md:border-0 md:bg-white md:text-sm md:font-medium"
         >
-          <li>
+          <!-- <li>
             <a
               href="#"
               class="block rounded bg-blue-700 py-2 pl-3 pr-4 text-white md:bg-transparent md:p-0 md:text-blue-700"
               aria-current="page"
               >Home</a
             >
-          </li>
-          <li>
-            <a
-              href="#"
-              class="block rounded py-2 pl-3 pr-4 text-gray-700 hover:bg-gray-100 md:p-0 md:hover:bg-transparent md:hover:text-blue-700"
-              >About</a
-            >
-          </li>
-          <li>
-            <a
-              href="#"
-              class="block rounded py-2 pl-3 pr-4 text-gray-700 hover:bg-gray-100 md:p-0 md:hover:bg-transparent md:hover:text-blue-700"
-              >Services</a
-            >
-          </li>
-          <li>
-            <a
-              href="#"
-              class="block rounded py-2 pl-3 pr-4 text-gray-700 hover:bg-gray-100 md:p-0 md:hover:bg-transparent md:hover:text-blue-700"
-              >Contact</a
-            >
-          </li>
+          </li> -->
         </ul>
       </div>
     </div>
